6eb906c59af1ec980c87c938a5c4624edf0a5d87
David Gifford <giff0065@algonquinlive.com>
2023-08-23 13:19:00 -0400

    hi again

---
+++ config.js
@@ -0,0 +1,32 @@
+import { defaultTheme } from 'vuepress'
+
+module.exports = {
+    lang: 'en-US',
+    title: `David Gifford's Portfolio`,
+    description: `David Gifford's Portfolio`,
+    theme: defaultTheme({
+        layout: 'CustomLayout',
+        navbar: [
+          {
+              text: 'Home',
+              link: '/',
+          },
+          {
+            text: 'Resume',
+            link: '/resume/',
+        },
+        {
+            text: 'Photography',
+            link: '/photography/',
+        },
+        {
+            text: 'Web',
+            link: '/web/',
+        },
+        {
+            text: 'Graphics',
+            link: '/graphics/',
+        }
+        ],
+    }),
+}
---
+++ docs/graphics/index.md
@@ -0,0 +1,16 @@
+---
+title: Graphics
+---
+
+# My Graphic Projects
+
+## Vector Images
+![Alt Text](.vuepress/img/graphic/Artboard-1.png)
+A logo for a D&D campaign
+![Alt Text](.vuepress/img/graphic/gimme_a.jpg)
+An Image used for an Assigment with a 3D glitch effect
+![Alt Text](.vuepress/img/graphic/swordinrock.png)
+A Sword in a rock
+## Logo Redesign
+![Alt Text](.vuepress/img/graphic/Logo-Slide-100.jpg)
+A logo redesgin for twelve28
