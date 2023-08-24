d36aaec45fcfcacbca671a63f8d7c4742278c998
David Gifford <giff0065@algonquinlive.com>
2023-08-24 14:53:53 -0400

    config rework

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,33 +1,29 @@
-import { defaultTheme } from 'vuepress'
-
 module.exports = {
-    lang: 'en-US',
-    title: 'My Portfolio',
-    description: 'Static Site Genrator',
-    base: '/portfoliosite/',
-    theme: defaultTheme({
-        layout: 'CustomLayout',
-        navbar: [
-          {
-              text: 'Home',
-              link: '/',
-          },
-          {
-            text: 'Resume',
-            link: '/resume/',
-        },
-        {
-            text: 'Photography',
-            link: '/photography/',
-        },
-        {
-            text: 'Web',
-            link: '/web/',
-        },
-        {
-            text: 'Graphics',
-            link: '/graphics/',
-        }
+    lang: 'en-US',                      // Language for the site
+    title: `David Gifford's Portfolio`, // Title of the site
+    description: `David Gifford's Portfolio`, // Description of the site
+    theme: defaultTheme({         
+        navbar: [                       // Configuration for the navigation bar links
+            {
+                text: 'Home',
+                link: '/',
+            },
+            {
+                text: 'Resume',
+                link: '/resume/',
+            },
+            {
+                text: 'Photography',
+                link: '/photography/',
+            },
+            {
+                text: 'Web',
+                link: '/web/',
+            },
+            {
+                text: 'Graphics',
+                link: '/graphics/',
+            }
         ],
     }),
-}
+};
