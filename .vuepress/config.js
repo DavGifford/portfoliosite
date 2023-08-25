eae498897b530602022cea89483e24eb8c8a32f8
David Gifford <giff0065@algonquinlive.com>
2023-08-24 23:10:47 -0400

    reworking the theme

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     lang: 'en-US',                      // Language for the site
     title: `David Gifford's Portfolio`, // Title of the site
     description: `David Gifford's Portfolio`, // Description of the site
-    theme: defaultTheme({         
+    themeConfig: ({         
         navbar: [                       // Configuration for the navigation bar links
             {
                 text: 'Home',
